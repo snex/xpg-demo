243da06f51139824fcc34ec75ed546704525e58c
David Havlicek <snex00@protonmail.com>
2023-05-23 13:52:13 -0700

    clearance requires ActionMailer bcuz dum

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,17 @@ class User < ApplicationRecord
   include Clearance::User
 
   has_many :orders
+
+  after_create :confirm_user
+
+  def send_confirmation_email#deliver_confirmation_email
+    # Do Nothing
+    # or MyMailer.deliver_thank_you self
+  end
+
+  private
+
+  def confirm_user
+    confirm_email!
+  end
 end
--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -2,7 +2,6 @@ Clearance.configure do |config|
   config.allow_sign_up = false
   config.cookie_expiration = ->(_cookies) { 1.year.from_now.utc }
   config.httponly = true
-  config.mailer_sender = 'snex@xens.org'
   config.password_strategy = Clearance::PasswordStrategies::BCrypt
   config.redirect_url = '/'
   config.rotate_csrf_on_sign_in = true
